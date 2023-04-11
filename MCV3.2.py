13f8863ed4ede44520d4d571cff7f381f93b2253
xx255q <42384590+xx255q@users.noreply.github.com>
2023-04-11 02:41:57 -0400

    MCV5.0
    n the main() function, the following changes were made:
    a. Removed the calls to move_2000ad(), first_scan(), and clean_folders() functions.
    b. Simplified the file processing logic by directly iterating over files in the source folder.
    
    In the script, the following functions were removed:
    a. move_2000ad()
    b. clean_title()
    c. clean_folders()
    
    In the script, the following items were removed:
    a. The unidecode import statement
    b. The LOG_FILE constant
    c. The log() function
    d. The time.sleep(3) line
    
    Added a new extract_folder_title() function to replace the clean_title() function.
    
    Updated the main() function to use the extract_folder_title() function when determining the new folder path.
    
    Replaced the log function calls with print statements to display the output.

--- MCV3.2.py
+++ MCV3.2.py
@@ -1,32 +1,8 @@
 import os
 import re
 import shutil
-import time
-from unidecode import unidecode
 
-LOG_FILE = 'move_comics.log'
-
-def log(message):
-    with open(LOG_FILE, 'a') as f:
-        f.write(f'{message}\n')
-    print(message)
-
-def move_2000ad():
-    source_folder = r"G:\Programs\Comics\Converted"
-    destination_folder = r"G:\Programs\Comics\Done\2000AD"
-
-    for root, dirs, files in os.walk(source_folder):
-        for file in files:
-            if file.startswith('2000AD prog'):
-                filepath = os.path.join(root, file)
-                if not os.path.exists(destination_folder):
-                    os.makedirs(destination_folder)
-                    log(f'Created new folder: {destination_folder}')
-
-                shutil.move(filepath, destination_folder)
-                log(f"Moved {file} to {destination_folder}")
-
-def first_scan():
+def main():
     source_folder = r"G:\Programs\Comics\Converted"
     destination_folder = r"G:\Programs\Comics\Done"
 
@@ -34,92 +10,42 @@ def first_scan():
         for file in files:
             if file.endswith('.cbr') or file.endswith('.cbz'):
                 filepath = os.path.join(root, file)
-                file_without_ext = os.path.splitext(file)[0]
+                folder_title = extract_folder_title(file)
+                new_folder_path = os.path.join(destination_folder, folder_title)
 
-                # Extract volume number from file name, if available
-                v_number_match = re.search(r'v(\d+)', file_without_ext)
-                v_number = None
-                if v_number_match:
-                    v_number = v_number_match.group(1)
-
-                main_title = re.sub(r'\d+.*', '', file_without_ext).strip()
-                cleaned_title = clean_title(main_title, v_number)
-
-                if not cleaned_title:
-                    cleaned_title = 'No Title'
-
-                new_folder_path = os.path.join(destination_folder, cleaned_title)
                 if not os.path.exists(new_folder_path):
                     os.makedirs(new_folder_path)
-                    log(f'Created new folder: {new_folder_path}')
+                    print(f'Created new folder: {new_folder_path}')
 
                 shutil.move(filepath, new_folder_path)
-                log(f"Moved {file} to {new_folder_path}")
+                print(f"Moved {file} to {new_folder_path}")
 
 
+def extract_folder_title(file):
+    # Remove file extension
+    cleaned_title = os.path.splitext(file)[0]
 
+    # Remove content inside square brackets and parentheses
+    cleaned_title = re.sub(r'\[.*?\]|\(.*?\)', '', cleaned_title)
 
-
-def clean_title(title, v_number=None):
-    # Step 1 - Remove content inside brackets, unless it starts with 'v' followed by digits
-    cleaned_title = re.sub(r'\[((?!v\d+).*?)\]', '', title)
-    print(f"Step 1 - Remove content inside brackets, unless it starts with 'v' followed by digits: {cleaned_title}")
-
-    # Step 2 - Remove content inside parentheses, unless it starts with 'v' followed by digits
-    cleaned_title = re.sub(r'\(((?!v\d+).*?)\)', '', cleaned_title)
-    print(f"Step 2 - Remove content inside parentheses, unless it starts with 'v' followed by digits: {cleaned_title}")
-
-    # Step 3 - Remove unwanted characters (keep only alphanumeric, whitespace characters, hyphen, and parentheses)
+    # Remove unwanted characters (keep only alphanumeric, whitespace characters, hyphen, and parentheses)
     cleaned_title = re.sub(r'[^\w\s\-\(\)]', '', cleaned_title)
-    print(f"Step 3 - Remove unwanted characters: {cleaned_title}")
 
-    # Step 4 - Remove any leading or trailing whitespace
+    # Remove issue numbers followed by a space and a year (4 digits) or directly followed by parentheses
+    cleaned_title = re.sub(r'(?<=\D)\d{1,3}(?=\s*\d{4}|\()', '', cleaned_title)
+
+    # Remove the year itself
+    cleaned_title = re.sub(r'\d{4}', '', cleaned_title)
+
+    # Remove any trailing or leading spaces
     cleaned_title = cleaned_title.strip()
-    print(f"Step 4 - Remove leading and trailing whitespace: {cleaned_title}")
 
-    # Step 5 - If there's a v_number available, add it after "v" if "v" is followed by a non-digit character or a space
-    if v_number:
-        cleaned_title = re.sub(r'(?<=v)(?=\D|$)', str(v_number), cleaned_title)
-        print(f"Step 5 - Add the v_number after 'v' if 'v' is followed by a non-digit character or a space: {cleaned_title}")
-
-    # Step 6 - Remove non-alphanumeric content inside parentheses
-    cleaned_title = re.sub(r'\((\W+?)\)', '', cleaned_title)
-    print(f"Step 6 - Remove non-alphanumeric content inside parentheses: {cleaned_title}")
-
-    # Remove specific words: "Europe Comics", "Black Label", etc.
-    cleaned_title = re.sub(r'Europe Comics|Black Label', '', cleaned_title)
-    print(f"Remove specific words: {cleaned_title}")
-
-    # Remove trailing parentheses, if any
-    cleaned_title = re.sub(r'\($', '', cleaned_title)
-    print(f"Remove trailing parentheses: {cleaned_title}")
-
-    # Remove any leading or trailing whitespace after removing the trailing parentheses
-    cleaned_title = cleaned_title.strip()
-    print(f"Remove leading and trailing whitespace after removing trailing parentheses: {cleaned_title}")
+    # Remove trailing issue numbers
+    cleaned_title = re.sub(r'\s+\d{1,3}$', '', cleaned_title)
 
     return cleaned_title
 
 
-def clean_folders():
-    root_folder = r"G:\Programs\Comics\Done"
-    for root, dirs, files in os.walk(root_folder):
-        for dir in dirs:
-            cleaned_title = clean_title(dir)
-            old_folder_path = os.path.join(root, dir)
-            new_folder_path = os.path.join(root, cleaned_title)
 
-            if old_folder_path != new_folder_path:
-                os.rename(old_folder_path, new_folder_path)
-                log(f"Renamed folder: {old_folder_path} to {new_folder_path}")
-
-
-# Make sure you have the 'unidecode' package installed
-# You can install it using: pip install unidecode
-
-move_2000ad()
-time.sleep(3)
-first_scan()
-clean_folders()
- 
-   
+if __name__ == "__main__":
+    main()
