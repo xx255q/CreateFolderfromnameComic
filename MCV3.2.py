dc5290845e150c173131990523ca3160d6bb84fd
xx255q <42384590+xx255q@users.noreply.github.com>
2023-04-11 07:16:59 -0400

    MCV 5.3
    Add error handling to prevent the script from crashing when encountering invalid file names or inaccessible folders

--- MCV3.2.py
+++ MCV3.2.py
@@ -17,8 +17,11 @@ def main():
                     os.makedirs(new_folder_path)
                     print(f'Created new folder: {new_folder_path}')
 
-                shutil.move(filepath, new_folder_path)
-                print(f"Moved {file} to {new_folder_path}")
+                try:
+                    shutil.move(filepath, new_folder_path)
+                    print(f"Moved {file} to {new_folder_path}")
+                except Exception as e:
+                    print(f"Error moving {file}: {str(e)}")
 
 
 def extract_folder_title(file):
@@ -31,8 +34,8 @@ def extract_folder_title(file):
     # Remove unwanted characters (keep only alphanumeric, whitespace characters, hyphen, and parentheses)
     cleaned_title = re.sub(r'[^\w\s\-\(\)]', '', cleaned_title)
 
-    # Remove issue numbers followed by a space and a year (4 digits) or directly followed by parentheses
-    cleaned_title = re.sub(r'(?<=\D)\d{1,3}(?=\s*\d{4}|\()', '', cleaned_title)
+    # Remove issue numbers followed by a year (4 digits)
+    cleaned_title = re.sub(r'\d{1,3}(?=\s*\d{4})', '', cleaned_title)
 
     # Remove the year itself
     cleaned_title = re.sub(r'\d{4}', '', cleaned_title)
@@ -40,8 +43,12 @@ def extract_folder_title(file):
     # Remove any trailing or leading spaces
     cleaned_title = cleaned_title.strip()
 
-    # Remove trailing issue numbers
-    cleaned_title = re.sub(r'\s+\d{1,3}$', '', cleaned_title)
+    # Remove issue numbers after volume number or any non-digit characters
+    cleaned_title = re.sub(r'(\D|\sv\d+)\s+\d+', r'\1', cleaned_title)
+
+    # Special case for "2000AD prog"
+    if "AD prog" in cleaned_title:
+        cleaned_title = cleaned_title.replace("AD prog", "2000AD")
 
     return cleaned_title
 
